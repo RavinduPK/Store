6a4ec49183705cecc7bc46a9e6f8056877dc01ef
Rama <ravindukalhara215@gmail.com>
2025-10-28 09:08:27 +0530

    update base name

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ const supabase = "https://stsbpkbhfvzdcmkmkiav.supabase.co";
 
 function App() {
   return (
-    <BrowserRouter basename='/Stores'>
+    <BrowserRouter basename='/Store'>
      <Toaster position='top-right'/>
       <Routes>
         {/* Redirect root path to /register */}
